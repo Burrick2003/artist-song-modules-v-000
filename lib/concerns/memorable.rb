8574518e386a9f8084486ba0302158753b4e0c16
Blakeley Bond <burrick2003@yahoo.com>
2017-11-19 23:31:01 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,14 +2,15 @@ module Memorable
 
 
 module InstanceMethods
-  def self.count
-    self.all.count
-  end
+
 end
 
 module ClassMethods
   def self.reset_all
     self.all.clear
+  end
+  def self.count
+    self.all.count
   end
 end
 
