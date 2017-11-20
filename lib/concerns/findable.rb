c7b731e0f5c3e47f8c457563cc6aee2f082958da
Blakeley Bond <burrick2003@yahoo.com>
2017-11-20 00:21:48 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,8 @@
 module Findable #Extendable
   def find_by_name(name)
-    #@@artists.detect{|a| a.name == name}
-    temp = ObjectSpace.each_object(self).to_a
-
-    temp.detect{|a| a.name == name}
+    #@@artists.detect{|a| a.name == name} #Original
+    #temp = ObjectSpace.each_object(self).to_a #This works but doesn't pass, as the object ID is different in the array for whatever reason
+    #temp.detect{|a| a.name == name}
 
   end
 
