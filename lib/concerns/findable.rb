f6f4aa937192a121e6a68443bfc1e51cc1703afd
Blakeley Bond <burrick2003@yahoo.com>
2017-11-20 00:18:09 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable #Extendable
   def find_by_name
     #@@artists.detect{|a| a.name == name}
-    ObjectSpace.each_object(self).to_a
+    ObjectSpace.each_object(self).to_a.detect{|a| a.name == name}
 
   end
 
